c1666237422566d4bc0f389477b6c96181e8fe47
Baymaxxxxx <pauliney3k@gmail.com>
2021-06-26 16:19:56 +0900

    fix mainpage

--- src/pages/Mainpage.tsx
+++ src/pages/Mainpage.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
 import Mainrightpage from '../components/MainRight';
 import Mainleftpage from '../components/MainLeft';
+import Footer from '../components/Footer';
 import './CSS/Mainpage.css'
-// function Mainpage() {
-//   return (
-//    <div className='MainContainer'>
-//      <Greetings name="Hello" onClick={onClick} />;
-//     <Mainleftpage message="Hello"/>
-//     <Mainrightpage message="i"/>
-//         </div>
-//   );
-// }
 
-const Mainpage = ()  => {
-    
+const Mainpage = () => {
+
     return (
 
         <div className='mainpage_container'>
-        <Mainleftpage message="TMT"/>
-        <Mainrightpage message="i"/>
+            <Mainleftpage />
+            <Mainrightpage message="i" />
+            <Footer />
         </div>
     )
 };
-  
+
 export default Mainpage;
