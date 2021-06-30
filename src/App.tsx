60002188914165a975dbb75667da42217bfa7a68
wonbeenna <nwbnwb@naver.com>
2021-06-30 18:52:27 +0900

    pr test

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,14 @@ import React from "react";
 import logo from "./logo.svg";
 // import './App.css';
 import Mainpage from "./pages/Mainpage";
-import Mypage from "./pages/Mypage";
 
 function App() {
   return (
     <div className="App">
       {/* <Mypage /> */}
-      {/* <Mainpage /> */}
+      <Mainpage />
       {/* <SignIn /> */}
-      <SignUp />
+      {/* <SignUp /> */}
     </div>
   );
 }
