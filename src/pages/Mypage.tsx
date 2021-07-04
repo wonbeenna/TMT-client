7dae28374750908eebf66c7cb19620cc078fca2a
Baymaxxxxx <pauliney3k@gmail.com>
2021-07-04 13:10:29 +0900

    fix mypage 0.2

--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -1,21 +1,10 @@
 import React, { useState } from "react";
-import DatePicker from "react-datepicker"
-import { DateRange } from 'react-date-range';
 import Map from '../components/Map'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import './CSS/Mypage.css'
-import 'react-date-range/dist/theme/default.css';
 
 const Mypage = () => {
-    // const [startDate, setStartDate] = useState(new Date())
-    const [state, setState] = useState([
-        {
-            startDate: new Date(),
-            endDate: null,
-            key: 'selection'
-        }
-    ]);
 
     return (
         <>
@@ -27,16 +16,6 @@ const Mypage = () => {
                 </div>
                 <div className="mypageMap">
                     <div className="calendar">
-                        달력자리
-                        {/* <Calendar onChange={(date: Date) => setDate(date)} /> */}
-                        <DateRange
-                            editableDateInputs={true}
-                            onChange={(item: any) => setState([item.selection])}
-                            moveRangeOnFirstSelection={false}
-                        // ranges={state}
-                        />
-                        {/* <DatePicker selected={startDate} onChange={(date: Date) => setStartDate(date)}
-                        /> */}
                     </div>
                     <div className="like">여행지like</div>
                 </div>
