32967179d6670be1c59a920b56e56b7edaa234d5
wonbeenna <nwbnwb@naver.com>
2021-07-14 01:44:36 +0900

    FixMap/0.4

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -15,7 +15,7 @@ const Map = (placedata: any) => {
   const { kakao } = window;
   const [map, setMap] = useState<any>(null);
   const [markerArr, setMarkerArr] = useState<any>([]);
-  const [pathArr, setPathArr] = useState<any>([]);
+  const [pathArr, setPathArr] = useState<any>({});
 
   // 지도 실행
   useEffect(() => {
@@ -30,8 +30,8 @@ const Map = (placedata: any) => {
     kakao.maps.load(() => {
       let container = document.getElementById("map");
       let options = {
-        center: new kakao.maps.LatLng(37.579698652999916, 126.97699720184801),
-        level: 10,
+        center: new kakao.maps.LatLng(36.579698652999916, 125.37699720184801),
+        level: 12,
       };
       let map = new kakao.maps.Map(container, options);
       setMap(map);
@@ -40,13 +40,16 @@ const Map = (placedata: any) => {
 
   // 지도 띄우기
   const viewMarker = () => {
+    if (markerArr.length > 0) {
+      removeMarker();
+    }
     let imageSrc = "./img/marker_map_icon.png";
     let imageSize = new kakao.maps.Size(50, 50);
     let markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
 
-    let bounds = new kakao.maps.LatLngBounds();
     const tempArr: any = [];
     const linePath: any = [];
+
     ListData[0]?.forEach((el: any) => {
       tempArr.push(
         new kakao.maps.Marker({
@@ -55,37 +58,31 @@ const Map = (placedata: any) => {
           image: markerImage,
         })
       );
-
-      linePath.push(new kakao.maps.LatLng(el.lat, el.long));
-
+      let bounds = new kakao.maps.LatLngBounds();
       bounds.extend(new kakao.maps.LatLng(el.lat, el.long));
-
-      let polyline = new kakao.maps.Polyline({
-        path: linePath,
-        strokeWeight: 5,
-        strokeColor: "#75B8FA",
-        strokeOpacity: 0.7,
-        strokeStyle: "solid",
-
-      });
-      polyline.setMap(map);
-
-      setMarkerArr(tempArr);
-      setPathArr(linePath);
+      linePath.push(new kakao.maps.LatLng(el.lat, el.long));
       map.setBounds(bounds, 90, 30, 10, 400);
     });
+    let polyline = new kakao.maps.Polyline({
+      path: linePath,
+      strokeWeight: 5,
+      strokeColor: "#75B8FA",
+      strokeOpacity: 0.7,
+      strokeStyle: "solid",
+    });
+    polyline.setMap(map);
+    setPathArr(polyline);
+    setMarkerArr(tempArr);
 
     function removeMarker() {
-      markerArr?.forEach((e: any) => e?.setMap(null));
-      ListData = [];
+      // markerArr?.forEach((e: any) => e?.setMap(null));
+      for (let i = 0; i < markerArr?.length; i++) {
+        markerArr[i]?.setMap(null);
+      }
+      pathArr?.setMap(null);
+      // ListData = [];
     }
-
-    removeMarker();
   };
-
-  console.log(ListData[0]);
-  console.log(pathArr);
-
 
   return (
     <div className="Map">
